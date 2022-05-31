089f4ed7968d4ad3f8558fec74b1efd43908b378
rrich-kray <rrich.kray@gmail.com>
2022-05-31 11:49:12 -0400

    added iframe

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,16 +5,16 @@ const Footer = () => {
   return (
     <div id="footer">
       <div id="footer-container-left">
-        {/* <iframe
+        <iframe
           title="map"
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3172.3321790987657!2d-122.01116604835985!3d37.33464377974313!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808fb596e9e188fd%3A0x3b0d8391510688f0!2sApple%20Park!5e0!3m2!1sen!2sus!4v1654010714566!5m2!1sen!2sus"
           width="600"
           height="450"
-          style="border:0;"
+          // style="border:0;"
           allowfullscreen=""
           loading="lazy"
           referrerpolicy="no-referrer-when-downgrade"
-        ></iframe> */}
+        ></iframe>
       </div>
       <div id="footer-container-right">
         <h1 id="contact">Contact Info:</h1>
