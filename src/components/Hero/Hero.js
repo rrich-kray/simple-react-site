aa7a03b3e476a6bc8286e6e36c9920fed1ea1db5
rrich-kray <rrich.kray@gmail.com>
2022-05-31 12:06:21 -0400

    added text to hero component

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,7 +4,15 @@ import "./Hero.css";
 const Hero = () => {
   return (
     <div id="hero">
-      <div id="hero-content-container"></div>
+      <div id="hero-content-container">
+        <h1>We Develop</h1>
+        <p>
+          Velit magna elit amet aliquip commodo anim labore consequat. Lorem do
+          aliquip pariatur pariatur et nostrud nostrud eiusmod minim officia ut
+          dolor. Tempor esse id officia sit reprehenderit.
+        </p>
+        <button>Learn More</button>
+      </div>
     </div>
   );
 };
